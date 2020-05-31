b2d664413b96eb877d4fb10563e70749afbacef7
Laith Harb <harblaith77@gmail.com>
2020-05-31 19:36:59 -0400

    Added log-in route and provided client with JWT upon successful authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database/db");
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const keys = require('../configs/dev')
 
 
 router.post("/signup", (req, res, next) => {
@@ -11,31 +13,70 @@ router.post("/signup", (req, res, next) => {
     .findOne({email: "harblaith77@gmail.com"})
     .then(user => {
         if(user){
-            return res.status(409).json({
+            res.status(409).json({
                 message: "email already exists"
+            })
+            return
+        } else {
+            bcrypt.hash("password", 10, (err, hash) => {
+                if(err){
+                    res.send(err)
+                } else {
+                    db.getDb()
+                    .collection("users")
+                    .insertOne({
+                        firstName: "Laith",
+                        lastName: "Harb",
+                        email: "harblaith77@gmail.com",
+                        password: hash
+                    })
+                    .then(response => {
+                        res.send(response)
+                    })
+                    .catch(error => {
+                        res.send(error)
+                    })
+                }
             })
         }
     })
 
-    bcrypt.hash("password", 10, (err, hash) => {
-        if(err){
-            res.send(err)
-        } else {
-            db.getDb()
-            .collection("users")
-            .insertOne({
-                firstName: "Laith",
-                lastName: "Harb",
-                email: "harblaith77@gmail.com",
-                password: hash
-            })
-            .then(response => {
-                res.send(response)
-            })
-            .catch(error => {
-                res.send(error)
-            })
+    
+})
+
+router.post("/login", (req, res, next) => {
+    
+    db.getDb()
+    .collection("users")
+    .findOne({
+        email: "harblaith77@gmail.com"
+    })
+    .then(user => {
+        if(!user){
+            return res.send("Auth Failed")
         }
+        bcrypt.compare("password", user.password, (err, result) => {
+            if(err){
+                return res.send("Auth failed")
+            }
+            if(result){
+                const token = jwt.sign(
+                    {
+                    email: user.email,
+                    userId: user._id
+                    },
+                    keys.JWT_KEY,
+                    {
+                        expiresIn: "1h"
+                    }
+                )
+                return res.json({
+                    message: "Auth successful",
+                    token
+                })
+            }
+            return res.send("Auth failed")
+        })
     })
 })
 
