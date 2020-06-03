b13758a5c866c70243d3c4179b5a763ffed0c647
Laith Harb <harblaith77@gmail.com>
2020-06-03 10:54:33 -0400

    Completed sign-up and login route

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,13 @@ const express = require("express")
 const router = express.Router()
 const {check, validationResult} = require("express-validator");
 const bcrypt = require("bcrypt");
-const db = require("../database/db")
+const db = require("../database/db");
+const keys = require("../config/dev");
+const jwt = require("jsonwebtoken")
 
 
-// Sign up route
-router.get("/signup", [
+// SIGN UP ROUTE 
+router.post("/signup", [
     check("firstName", "First name is required")
         .not()
         .isEmpty(),
@@ -15,45 +17,106 @@ router.get("/signup", [
         .isEmpty(),
     check("email", "Please provide a valid email")
         .isEmail(),
-    check("password", "The must be greater than 6 and smaller than 50 character")
+    check("password", "Password must be greater than 6 and less than 50 characters")
         .isLength({min: 6, max: 50})
-], (req, res, next) => {
-   // CHECK IF OUR INPUT FIELDS ARE VALID
-   const errors = validationResult(req);
-   if(errors.errors.length){
-       return res.status(400).json(errors.array())
-   }
+], (req, res) => {
+    // Validate user input 
+    const {firstName, lastName, email, password} = req.body
+    const {errors} = validationResult(req)
+    if(errors.length){
+        return res.status(409).json({
+            ourErrors : errors
+        })
+    }
 
-   const {firstName, lastName, email, password} = req.body
+    // Validate the user email doesnt already exist 
+    db.getDb()
+    .collection("users")
+    .findOne({email})
+    .then(user => {
+        if(user) return res.status(400).json({message: "User is already registered"})
+        // hash the password  
+        bcrypt.hash(
+            password,
+            10,
+            (err, hash) => {
+                if(err){
+                    return res.status(500).json({message: "Hashing went wrong"})
+                }
+                // store the user document
+                db.getDb()
+                .collection("users")
+                .insertOne({
+                    firstName,
+                    lastName,
+                    email,
+                    password: hash
+                })
+                .then(user => {
+                   // return a json web token 
+                   const token = jwt.sign(
+                       {email: user.email, id: user._id},
+                       keys.JWT_Secret,
+                        {expiresIn: "1h"}
+                   )
+                   return res.status(200).json({
+                       message: "User created",
+                       token
+                   })
+                })
+                .catch(err => {
+                    return res.status(500).json({message: "User generation went wrong"})
+                })
+            }
+        )
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "Something went wrong"
+        })
+    })
+  
+})
 
-   // HASH OUR PASSWORD
-   bcrypt.hash(password, 10, (err, hash) => { 
-       if(err){
-           return res.status(400).json({
-               message: "Something went wrong"
-           })
-       }
-       // SAVE THE USER DOCUMENT INTO MONGODB
-       db.getDb()
-       .collection("users")
-       .insertOne({
-           firstName,
-           lastName,
-           email,
-           password: hash
-       })
-       .then(response => {
-           return res.status(200).json(response)
-       })
-       .catch(err => {
-           res.status(400).json({
-             message: "Something went wrong"
-           })
-       })
-   })
 
-   
-   
+
+// LOG USER IN ROUTE
+router.post("/login", [
+    check("email", "Please provide a valid email")
+        .isEmail(),
+    check("password", "Please provide a password")
+        .not()
+        .isEmpty()
+], (req, res) => {
+    const {errors} = validationResult(req)
+    if(errors.length) return res.status(400).json({message : "Auth Failed"})
+
+    const {password, email} = req.body
+    // CHECK IF EMAIL IS IN DATABASE    
+    db.getDb()
+    .collection("users")
+    .findOne({email})
+    .then(user => {
+        if(!user) return res.status(400).json({message : "Auth Failed"})
+        bcrypt.compare(password, user.password, (err, result) => {
+            if(!result) return res.status(400).json({message : "Auth Failed"})
+            // RETURN A JSON WEB TOKEN
+            const token = jwt.sign(
+                {email: user.email, id: user._id},
+                keys.JWT_Secret,
+                 {expiresIn: "1h"}
+            )
+            return res.status(200).json({
+                message: "Auth Successful",
+                token
+            })
+        })
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: "Auth Failed"
+        })
+    })
 })
 
 
